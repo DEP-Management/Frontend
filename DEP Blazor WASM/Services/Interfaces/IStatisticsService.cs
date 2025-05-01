f21b277adc44e40b932c205c7632ce25649a5f81
Jimmy <swattlebagger@gmail.com>
2025-05-01 12:00:17 +0200

    statistics page

---
+++ DEP Blazor WASM/Services/Interfaces/IStatisticsService.cs
@@ -0,0 +1,12 @@
+﻿using DEP_Blazor_WASM.Entities.ViewModels;
+
+namespace DEP_Blazor_WASM.Services.Interfaces
+{
+    public interface IStatisticsService
+    {
+        Task<List<PersonPerDepartmentViewModel>> GetPersonsPerDepartmentAsync();
+        Task<List<PersonPerDepartmentViewModel>> GetPersonsPerDepartmentByModuleIdAsync(int moduleId);
+        Task<List<PersonPerLocationViewModel>> GetPersonsPerLocationAsync();
+        Task<List<CourseStatusCountViewModel>> GetPersonsPerCourseStatusByModuleIdAsync(int moduleId);
+    }
+}
